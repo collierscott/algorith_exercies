a64ba8d3bb44b2680a12a2ff18cd26e72c771839
Scott Collier <scott.collier@umuc.edu>
2019-02-04 15:28:07 +0100

    Reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n.toString().split('').reverse().join('');
+  return Math.sign(n) * parseInt(reversed);
+}
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//   let num = n.toString();
+//
+//   const revNum = num.split('').reduce((rev, char) => (
+//     char + rev
+//   ), '');
+//
+//   return Math.sign(n) * parseInt(revNum);
+// }
