52f1696ea988caeb2e665c1044e6bc5aa49aae91
Scott Collier <scott.collier@umuc.edu>
2019-02-04 14:14:46 +0100

    initial

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,49 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+function reverse(str) {
+  // Takes starting argument (empty string)
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   let reversed = '';
+//   // for(let i = str.length - 1; i >= 0; i--) {
+//   //   reversed += str.charAt(i);
+//   // }
+//
+//   // Alternate way to loop
+//   for(let character of str) {
+//     reversed = character + reversed;
+//   }
+//
+//   return reversed;
+// }
+
+// function reverse(str) {
+//   let low = 0;
+//   let high = str.length - 1;
+//   let arry = str.split('');
+//
+//   while(low < high) {
+//     let temp = arry[low];
+//     arry[low] = arry[high];
+//     arry[high] = temp;
+//     low++;
+//     high--;
+//   }
+//
+//   return arry.join('');
+// }
