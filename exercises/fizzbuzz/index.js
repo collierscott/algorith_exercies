9f69933942d1fb2cf7b381af9353b820a9508216
Scott Collier <scott.collier@umuc.edu>
2019-02-05 15:30:16 +0100

    classic fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,18 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for(let i = 1; i <= n; i++) {
+    if(0 === i % 3 && 0 === i % 5) {
+      console.log('fizzbuzz');
+    } else if(0 === i % 3) {
+      console.log('fizz');
+    } else if(0 === i % 5) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
