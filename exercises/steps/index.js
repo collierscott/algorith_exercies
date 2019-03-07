696ac8cf409029f6f7e2bf4facc33b4d25beab46
Scott Collier <scott.collier@umuc.edu>
2019-03-07 15:39:01 +0100

    Steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,37 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// recursion
+function steps(n, row = 0, stair = '') {
+  if(n === row) return;
+
+  if(n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+
+  if(stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
+}
 
 module.exports = steps;
+
+// function steps(n) {
+//   for(let i = 0; i < n; i++) {
+//     let str = '';
+//     for(let j = 0; j < n; j++) {
+//       if(j <= i) {
+//         str += '#';
+//       } else {
+//         str += ' ';
+//       }
+//     }
+//     console.log(str);
+//   }
+// }
